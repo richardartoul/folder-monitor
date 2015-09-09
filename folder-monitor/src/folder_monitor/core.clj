a691864cf53ce16262cf75a58eb78966519cd3ad
Richard Artoul <richardartoul@gmail.com>
2015-09-08 21:19:14 -0700

    [Feature] MVP - Program accomplishes main task

--- folder-monitor/src/folder_monitor/core.clj
+++ folder-monitor/src/folder_monitor/core.clj
@@ -1,39 +1,42 @@
+; clojure-watch is a simple wrapper around the Java 7 WatchEvent API
+; clojure.java.io is the clojure wrapper around the Java filesystem API
 (ns folder-monitor.core
   (:gen-class)
-  (:require [clojure-watch.core :refer [start-watch]]))
+  (:require [clojure-watch.core :refer [start-watch]])
+  (:require [clojure.java.io :as io]))
 
 ; regex for identifying misnamed sookasa files
 ; also includes capturing groups so that filenames can be rearranged programatically
-(def sookasa-regex #"([^.]*)(\..*)(\(.*conflicted copy \d{4}-\d{2}-\d{2}\))\.sookasa")
+(def sookasa-regex #"([^.]*)(\..+)(\(.*conflicted copy \d{4}-\d{2}-\d{2}\))\.sookasa")
 
+; callback function is called everytime a file is created or modified
 (defn rename-file
   "Renames incorrectly named files to their proper form"
   [event filename]
   ; print the event and filename to the console - change this to a log file
   (println event filename)
   
-  ; destructuring to break apart regex groups
-  ; full-filename is the full filename
-  ; short-filename is the name of the file without the conflict parens and extensions
-  ; extension is the files extension (other than .sookasa)
-  ; conflict-string is the (users conflicted copy...) string
-  (let [[full-filename short-filename extension conflict-string]
-         (re-matches sookasa-regex filename)]
-    (println short-filename)
-    (println extension)
-    (println conflict-string)))
+  ; only do something if the regex pattern is found
+  (if (re-matches sookasa-regex filename)
+    ; destructuring to break apart regex groups
+    ; full-filename is the full filename
+    ; short-filename is the name of the file without the conflict parens and extensions
+    ; extension is the files extension (other than .sookasa)
+    ; conflict-string is the (users conflicted copy...) string
+    (let [[full-filename short-filename extension conflict-string]
+           (re-matches sookasa-regex filename)]
+      (def file-to-create (str short-filename conflict-string extension ".sookasa"))
+      ; rename the file
+      (.renameTo (io/file full-filename) 
+                 (io/file file-to-create)))))
 
-  ; see if the modified files name matches the incorrect pattern
-  ; (println 
-  ;   (re-matches sookasa-regex filename)))
-
+; main function that gets invoked when application starts
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!")
-  ; Starst watching the folder and listening for changes
-  (start-watch [{:path "/home/richie/Desktop/sookasa"
-                 :event-types [:create :modify :delete]
+  ; expects one argument: Which path to watch for file changes
+  [watch-folder & args]
+  ; Starts watching the folder and listening for changes
+  (start-watch [{:path watch-folder
+                 :event-types [:create :modify]
                  :bootstrap (fn [path] (println "Starting to watch " path))
                  :callback rename-file
                  :options {:recursive true}}]))
