816ea261d0d247a374ebe30615dc6ab0f45c3d8b
Richard Artoul <richardartoul@gmail.com>
2015-09-11 01:45:08 -0700

    [Feature] Updated powershell script

--- test/folder_monitor/file_overload.ps1
+++ test/folder_monitor/file_overload.ps1
@@ -3,12 +3,18 @@ param (
   [string]$NUMFILES
 )
 
+$FILES=@(test_files/testImage.jpg, test_files/testVideo.mp4, test_files/testWordDoc.doc)
+
 for ($i=1; $i < $NUMFILES; $i++) {
   $FILENAME="file$i.ext(Richie's conflicted copy 2015-09-08).sookasa"
-  FOR %%G IN (test_files/testImage.jpg test_files/testVideo.mp4 test_files/testWordDoc.doc) DO copy %%G "$PATH/$FILENAME"
+  ForEach ($ITEM in $FILES) {
+    copy $ITEM "$PATH/$FILENAME"
+  }
 }
 
 for ($i=1; $i < $NUMFILES; $i++) {
   $FILENAME="file$i.ext(Richie's conflicted copy 2015-09-08).sookasa"
-  FOR %%G IN (test_files/testImage.jpg test_files/testVideo.mp4 test_files/testWordDoc.doc) DO copy %%G "$PATH/nested/$FILENAME"
+ ForEach ($ITEM in $FILES) {
+    copy $ITEM "$PATH/nested/$FILENAME"
+  }
 }
