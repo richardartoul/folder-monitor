373abeb239448936262cdab51f1962ceab720b53
Richard Artoul <richardartoul@gmail.com>
2015-09-11 02:03:26 -0700

    [Feature] Powershell script creates nested folder

--- test/folder_monitor/file_overload.ps1
+++ test/folder_monitor/file_overload.ps1
@@ -3,7 +3,7 @@ param (
   [string]$NUMFILES
 )
 
-$FILES=(test_files/testImage.jpg, test_files/testVideo.mp4, test_files/testWordDoc.doc)
+$FILES=@("test_files/testImage.jpg", "test_files/testVideo.mp4", "test_files/testWordDoc.doc" )
 
 for ($i=1; $i -lt $NUMFILES; $i++) {
   $FILENAME="file$i.ext(Richie's conflicted copy 2015-09-08).sookasa"
@@ -12,6 +12,7 @@ for ($i=1; $i -lt $NUMFILES; $i++) {
   }
 }
 
+new-item "$PATH\nested" -itemtype directory
 for ($i=1; $i -lt $NUMFILES; $i++) {
   $FILENAME="file$i.ext(Richie's conflicted copy 2015-09-08).sookasa"
  ForEach ($ITEM in $FILES) {
