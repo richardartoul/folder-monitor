b2b3431545bd2c21a1e34331eacca1d8f0e3c78c
Richard Artoul <richardartoul@gmail.com>
2015-09-11 01:49:11 -0700

    [Refactor] Fixed a typo in powershell script

--- test/folder_monitor/file_overload.ps1
+++ test/folder_monitor/file_overload.ps1
@@ -3,7 +3,7 @@ param (
   [string]$NUMFILES
 )
 
-$FILES=@(test_files/testImage.jpg, test_files/testVideo.mp4, test_files/testWordDoc.doc)
+$FILES=(test_files/testImage.jpg, test_files/testVideo.mp4, test_files/testWordDoc.doc)
 
 for ($i=1; $i -lt $NUMFILES; $i++) {
   $FILENAME="file$i.ext(Richie's conflicted copy 2015-09-08).sookasa"
