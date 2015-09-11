080f953ec7b1e731db61d26134919eff3dd18579
Richard Artoul <richardartoul@gmail.com>
2015-09-11 01:46:25 -0700

    [Refactor] Fixed a typo in powershell script

--- test/folder_monitor/file_overload.ps1
+++ test/folder_monitor/file_overload.ps1
@@ -5,14 +5,14 @@ param (
 
 $FILES=@(test_files/testImage.jpg, test_files/testVideo.mp4, test_files/testWordDoc.doc)
 
-for ($i=1; $i < $NUMFILES; $i++) {
+for ($i=1; $i -lt $NUMFILES; $i++) {
   $FILENAME="file$i.ext(Richie's conflicted copy 2015-09-08).sookasa"
   ForEach ($ITEM in $FILES) {
     copy $ITEM "$PATH/$FILENAME"
   }
 }
 
-for ($i=1; $i < $NUMFILES; $i++) {
+for ($i=1; $i -lt $NUMFILES; $i++) {
   $FILENAME="file$i.ext(Richie's conflicted copy 2015-09-08).sookasa"
  ForEach ($ITEM in $FILES) {
     copy $ITEM "$PATH/nested/$FILENAME"
